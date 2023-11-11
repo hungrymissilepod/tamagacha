1ed884deb406f92b48390f67b198fd7375a35bf8
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-11-11 21:00:13 +0000

    chore: Rename Android app id

--- android/app/src/main/kotlin/com/example/flutter_app_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app_template/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app_template
+package com.otterlygood.tamagacha
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,6 @@ import 'package:flutter_app_template/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-/*
-Note: In order to publish this app please remember to change the application ID
-
-Android:
-Change this value 'com.example.flutter_app_template'
-
-iOS:
-Change this value 'com.otterlygood.tamagacha'
-
-Linux:
-Change this value 'com.example.flutter_app_template'
-
-MacOS:
-Change this value 'com.otterlygood.tamagacha' and 'com.otterlygood.tamagacha.RunnerTests'
-
-Windows:
-Change these values in Runner.rc:
-'com.example'
-'flutter_app_template'
-*/
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
